05dba7769b7ea08e26410cb4ebee82bcdd5ed9c3
梁哲 <603285292@qq.com>
2015-10-22 21:53:32 +0800

    一些L通道的SR结果

---
+++ bicubic_2d.py
@@ -0,0 +1,95 @@
+# coding:utf-8
+__author__ = 'liangz14'
+
+import math
+import numpy as np
+from skimage import color
+
+from scipy.misc  import imresize
+
+from skimage import data
+
+import matplotlib.pyplot as plt
+#bicubic params
+def S(x,a=-0.5):
+    if x<1 and x>=0:
+        return (a+2)*np.abs(x)**3-(a+3)*np.abs(x)**2+1
+    if x<2 and x>=1:
+        return a*np.abs(x)**3-5*a*np.abs(x)**2+8*a*np.abs(x)-4*a
+    else:
+        return 0
+
+'''
+    this method is for 2d array in float 三线性差值是使用最近的16个店进行插值
+    src_img :float image 浮点数图像
+    scale: float scale for x and y 浮点数倍数
+'''
+
+def bicubic2d(src_img,scale):
+    scale = scale*1.0  #we need a float scale
+    src_img = np.asarray(src_img,dtype ='float64') #we need a float 2d array
+    xlen = src_img.shape[1]
+    ylen = src_img.shape[0]
+    xlenh = int(xlen * scale)
+    ylenh = int(ylen * scale)
+    dst_img = np.zeros((ylenh, xlenh))
+    for i in range(ylenh):
+        for j in range(xlenh):
+            #we find the nearest 16 points for every points in dst image
+            tmp_i = i/scale
+            tmp_j = j/scale
+            src_i = np.floor(tmp_i)
+            src_j = np.floor(tmp_j)
+
+            v = tmp_i - src_i
+            u = tmp_j - src_j
+            #范围限定
+            if src_i == 0:
+                src_i = 1
+            if src_j == 0:
+                src_j = 1
+            if src_j+3 - xlen >0 :
+                src_j = xlen - 3
+            if src_i+3 - ylen >0 :
+                src_i = ylen - 3
+
+            patch = src_img[src_i-1:src_i+3 ,src_j-1 :src_j+3]
+
+            A = np.asarray([S(1+v),S(v),S(1-v),S(2-v)],dtype='float64')
+            B = np.asarray([S(1+u),S(u),S(1-u),S(2-u)],dtype='float64')
+
+            tempA = np.dot(A,patch)
+            dst_img[i,j] = np.dot(tempA,B)
+    return  dst_img
+
+def psnr( img1,img2):
+    mse = np.mean( (img1 - img2) ** 2 )
+    if mse == 0:
+        return 100
+    return 20 * math.log10(np.max(img1) / math.sqrt(mse))
+
+def main():
+    src_rgb_img = data.lena()
+    #src_rgb_img = io.imread('Child_input.png')
+
+    src_Lab_img = color.rgb2lab(src_rgb_img)
+    src_Lab_img_L = src_Lab_img[:,:,0]
+
+    src_Lab_img_a = src_Lab_img[:,:,1]
+    src_Lab_img_b = src_Lab_img[:,:,2]
+
+    #down_sampling with scale = 0.5
+    scale = 0.25
+    src = bicubic2d(src_Lab_img_L,scale)*2.55
+
+    #up_sampling with scale = 2.0 to source image by different method
+    dst_me = bicubic2d(src,4.0)
+    dst_other = imresize(src,4.0,interp='bicubic')#this method result is int and in [0,255]
+
+    #evalue different with PSNR MY result is better
+    print psnr(dst_me/2.55,src_Lab_img_L)
+    print psnr(dst_other/2.55,src_Lab_img_L)
+
+    plt.imshow(dst_me,interpolation="none")
+    plt.show()
+
--- getSuper.py
+++ getSuper.py
@@ -9,24 +9,25 @@ import scipy.io
 from scipy.misc  import imresize
 from scipy.signal import convolve2d
 from sklearn.externals import joblib
+from skimage import data
 #求解线性方程组：
 from sklearn import linear_model
-
+import bicubic_2d
 import matplotlib.pyplot as plt
 #超分辨率处理函数
-def SR(img,Dh, Dl,l_da=0.0001, scale=3.0,patch_sizel=3.0, overlap=1.0 ,img_size=1.0):
+def SR(img,Dh, Dl,l_da=0.00005, scale=3.0,patch_sizel=3.0, overlap=1.0 ,img_size=1.0):
     #l_da是经验值
-
     #得到特征域
     feat_scale=2.0
     patch_sizeh = patch_sizel*scale
     patch_sizem = patch_sizel*feat_scale
 
-    imgm = imresize(img, feat_scale,'bicubic')
-    imgh = np.zeros((img.shape[0]*3,img.shape[1]*3))
+    imgm = bicubic_2d.bicubic2d(img, feat_scale)
+    imgh = np.zeros((img.shape[0]*3,img.shape[1]*3),dtype='float64')
+    img_bicubic = bicubic_2d.bicubic2d(img, 3.0)
     normalizationMat = np.zeros((img.shape[0]*3,img.shape[1]*3))
 
-    print imgm.shape , imgh.shape
+    print img.shape ,imgm.shape , imgh.shape
     f1=np.asarray([[-1,0,1]],dtype='float64')
     f2=np.asarray([[-1],[0],[1]],dtype='float64')
     f3=np.asarray([[1,0,-2,0,1]],dtype='float64')
@@ -39,8 +40,13 @@ def SR(img,Dh, Dl,l_da=0.0001, scale=3.0,patch_sizel=3.0, overlap=1.0 ,img_size=
     F[:,:,2]= convolve2d(imgm,f3,mode='same')
     F[:,:,3]= convolve2d(imgm,f4,mode='same')
 
-    xgrid = np.ogrid[int(patch_sizel/2.0):img.shape[1]-patch_sizel:patch_sizel - overlap]
-    ygrid = np.ogrid[int(patch_sizel/2.0):img.shape[0]-patch_sizel:patch_sizel - overlap]
+    xgrid = np.ogrid[np.ceil(patch_sizel/2.0):img.shape[1]-patch_sizel:patch_sizel - overlap]
+    ygrid = np.ogrid[np.ceil(patch_sizel/2.0):img.shape[0]-patch_sizel:patch_sizel - overlap]
+    xgrid = np.asarray(xgrid)
+    ygrid = np.asarray(ygrid)
+
+    xgrid = np.append(xgrid,img.shape[1]-patch_sizel)
+    ygrid = np.append(ygrid,img.shape[0]-patch_sizel)
     xgridm = (xgrid-1)*feat_scale +1
     ygridm = (ygrid-1)*feat_scale+1
     xgridh = (xgrid-1)*scale+1
@@ -55,7 +61,8 @@ def SR(img,Dh, Dl,l_da=0.0001, scale=3.0,patch_sizel=3.0, overlap=1.0 ,img_size=
             patchm = imgm[y:y+patch_sizem, x:x+patch_sizem]
 
             avg_patch = np.mean(patchm)
-            featpatch = np.transpose(F[y:y+patch_sizem, x:x+patch_sizem,:],axes=(2,1,0)).reshape(patch_sizem*patch_sizem*4)
+
+            featpatch = np.transpose(F[y:y+patch_sizem, x:x+patch_sizem,:],axes=(2,0,1)).reshape(patch_sizem*patch_sizem*4)
             normalization_m = math.sqrt(np.sum(featpatch**2))
 
             yy=[]
@@ -66,7 +73,7 @@ def SR(img,Dh, Dl,l_da=0.0001, scale=3.0,patch_sizel=3.0, overlap=1.0 ,img_size=
             clf = linear_model.Lasso(alpha=l_da)
             clf.fit(Dl, yy)
             w = clf.coef_
-            #print x,y,np.sum(np.abs(w))
+
             patchh=[]
             if normalization_m > 1:
                     patchh = np.dot(Dh,w)*normalization_m
@@ -74,28 +81,39 @@ def SR(img,Dh, Dl,l_da=0.0001, scale=3.0,patch_sizel=3.0, overlap=1.0 ,img_size=
                     patchh = np.dot(Dh,w)
 
             patchh = patchh.reshape([patch_sizeh, patch_sizeh]) + avg_patch
-            patchh = np.transpose(patchh)
 
             imgh[yh:yh+patch_sizeh, xh:xh+patch_sizeh]= imgh[yh:yh+patch_sizeh, xh:xh+patch_sizeh] + patchh
             normalizationMat[yh:yh+patch_sizeh, xh:xh+patch_sizeh]= normalizationMat[yh:yh+patch_sizeh, xh:xh+patch_sizeh]+ 1
             print x,y
+
+    mask = normalizationMat == 0
+    normalizationMat[mask] = 1
+
     imgh = imgh/normalizationMat
-    joblib.dump(imgh, 'img_h2.pkl')
+    imgh[mask] = img_bicubic[mask]
+    joblib.dump(imgh, 'img_h0005.pkl')
     return imgh
+
+
 def readSR():
-    return joblib.load('img_h2.pkl')
+    return joblib.load('img_h3.pkl')
+def psnr( img1,img2):
+    mse = np.mean( (img1 - img2) ** 2 )
+    if mse == 0:
+        return 100
+    return 20 * math.log10(np.max(img1) / math.sqrt(mse))
 
 #加载字典
 mat = scipy.io.loadmat('./Dictionary/dictionary.mat')
 Dl_dict = np.asarray( mat['Dl'],dtype='float64')
 Dh_dict = np.asarray( mat['Dh'],dtype='float64')
-#print sc_dict.shape
 
 #提取出L通道进行计算其他通道使用Bicubic进行计算
 src_rgb_img = io.imread('./Data/Child_input.png')
-#src_rgb_img = src_rgb_img[30:70,22:60,:]
-print src_rgb_img.shape
+#src1 = data.lena()
+#src =src1[240:300,238:300,:]
 #需要变称散的倍数才能处理
+#src_rgb_img = bicubic_2d.bicubic2d(src,1/3.0)
 src_rgb_img = src_rgb_img[:src_rgb_img.shape[0]-src_rgb_img.shape[0]%3,:src_rgb_img.shape[1]-src_rgb_img.shape[0]%3,:]
 #print src_rgb_img.shape
 
@@ -104,18 +122,19 @@ src_Lab_img_L = src_Lab_img[:,:,0]
 src_Lab_img_a = src_Lab_img[:,:,1]
 src_Lab_img_b = src_Lab_img[:,:,2]
 
-dst_Lab_img_L = readSR()
-#dst_Lab_img_L = SR(src_Lab_img_L,Dh_dict,Dl_dict)
+#不同的L通道来源
+#dst_Lab_img_L = readSR()
+#m = dst_Lab_img_L<0
+#dst_Lab_img_L[m] = 0
+#print np.max(dst_Lab_img_L),np.min(dst_Lab_img_L)
+dst_Lab_img_L = SR(src_Lab_img_L,Dh_dict,Dl_dict)
 #dst_Lab_img_L = imresize(src_Lab_img_L,3.0,'bicubic')
-print np.max(src_Lab_img_L),np.min(src_Lab_img_L)
-dst_Lab_img_L= dst_Lab_img_L/255.0*(np.max(src_Lab_img_L)-np.min(src_Lab_img_L))+np.min(src_Lab_img_L)
+#dst_Lab_img_L= dst_Lab_img_L/255.0*(np.max(src_Lab_img_L)-np.min(src_Lab_img_L))+np.min(src_Lab_img_L)
 
 dst_Lab_img_a = imresize(src_Lab_img_a,3.0,'bicubic')
-print np.max(src_Lab_img_a),np.min(src_Lab_img_a)
 dst_Lab_img_a= dst_Lab_img_a/255.0*(np.max(src_Lab_img_a)-np.min(src_Lab_img_a))+np.min(src_Lab_img_a)
 
 dst_Lab_img_b = imresize(src_Lab_img_b,3.0,'bicubic')
-print np.max(src_Lab_img_b),np.min(src_Lab_img_b)
 dst_Lab_img_b= dst_Lab_img_b/255.0*(np.max(src_Lab_img_b)-np.min(src_Lab_img_b))+np.min(src_Lab_img_b)
 
 img_lab = np.zeros((dst_Lab_img_L.shape[0],dst_Lab_img_L.shape[1],3))
@@ -123,9 +142,12 @@ img_lab[:,:,0] = dst_Lab_img_L
 img_lab[:,:,1] = dst_Lab_img_a
 img_lab[:,:,2] = dst_Lab_img_b
 
+dst = color.lab2rgb(img_lab)
+#src = src[:dst.shape[0],:dst.shape[1]]
 
-img = color.lab2rgb(img_lab)
-print np.max(img),np.min(img)
-plt.imshow(img)
+#print np.mean(dst[10:-15,10:-15]*255-src[10:-15,10:-15])
+
+#print psnr(dst[10:-15,10:-15]*255,src[10:-15,10:-15])
+plt.imshow(dst,interpolation="none")
 
 plt.show()
